e4c9d4779feb3434cd518e0f1bcd819b19a946d1
Jsantos20201 <72623931+Jsantos20201@users.noreply.github.com>
2024-03-02 17:39:17 -0700

    adding to repo

---
+++ Edited Snake Game/Edited Snake Game/JavaScript/speed.js
@@ -0,0 +1,30 @@
+import { mySel, mySelAll } from "./constants.js";
+import { startGame } from "./game.js";
+
+var level = "";
+var scoreCounterUpto = 1;
+var snakeSpeed;
+
+mySel(".div").classList.add("d-none");
+mySelAll(".sbtn").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.classList.contains("easy")) {
+      level = "easy";
+      scoreCounterUpto = 5;
+      snakeSpeed = 900;
+    } else if (e.target.classList.contains("medium")) {
+      level = "medium";
+      scoreCounterUpto = 7;
+      snakeSpeed = 400;
+    } else if (e.target.classList.contains("hard")) {
+      level = "hard";
+      scoreCounterUpto = 10;
+      snakeSpeed = 100;
+    } else if (e.target.classList.contains("vhard")) {
+      level = "vhard";
+      scoreCounterUpto = 15;
+      snakeSpeed = 40;
+    }
+    startGame();
+  });
+});
---
+++ Edited Snake Game/Edited Snake Game/JavaScript/utils.js
@@ -0,0 +1,2 @@
+import { numOfRow } from "./constants.js";
+export const randomRow = () => Math.floor(Math.random() * numOfRow);
